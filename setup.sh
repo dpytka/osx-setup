0c6a876ce3f596f25f7c2ee77e4d00253c44b412
Dawid Pytka <dpytka@gmail.com>
2025-10-14 15:18:41 +0200

    Add ohmyzsh plugins

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ brew bundle install
 if [ ! -d ~/.oh-my-zsh ]; then
   echo "Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+  git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 fi
 
 zsh defaults
